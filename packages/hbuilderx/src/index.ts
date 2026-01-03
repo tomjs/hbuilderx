f8a9a0b29c75509627612454a2acb5ddf7a52334
Tom Gao <tom@tomgao.cc>
2026-01-03 12:12:48 +0800

    feat(hbuilderx): 新增初始插件方法

--- packages/hbuilderx/src/index.ts
+++ packages/hbuilderx/src/index.ts
@@ -1,4 +1,15 @@
+import type { ExtensionContext } from 'hbuilderx';
+import { setContext } from './ctx';
+
 export * from './constants';
 export * from './ctx';
+
+/**
+ * 初始插件
+ * @param {ExtensionContext} ctx 插件上下文
+ */
+export function initExtension(ctx: ExtensionContext) {
+  setContext(ctx);
+}
 
 export default {};
